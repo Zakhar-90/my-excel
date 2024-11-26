927b51182df1d24491241f69cd26d9a5b9fbaf6f
Vladimir Zakharchenko <vladimirzax@mail.ru>
2024-11-26 10:05:52 +0300

    Refact

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,23 +16,24 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
+    const vector = event.target.dataset.resize;
+    console.log(vector);
     if (event.target.dataset.resize) {
       const $resizer = $(event.target);
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
-      const nameColumn = $resizer.$el.dataset.column;
-      const listCellColumn = document.querySelectorAll(
-          `[data-name-column="${nameColumn}"]`
+      const cells = this.$root.findAll(
+          `[data-col="${$parent.data.col}"]`
       );
 
       document.onmousemove = (e) => {
         const delta = e.pageX - coords.right;
-        const value = `${coords.width + delta}px`;
+        const value = coords.width + delta;
 
-        $parent.$el.style.width = value;
+        $parent.$el.style.width = `${value}px`;
 
-        listCellColumn.forEach((cell) => {
-          cell.style.width = value;
+        cells.forEach((cell) => {
+          cell.style.width = `${value}px`;
           cell.style.borderRight = '2px solid #3c74ff';
         });
       }
@@ -40,10 +41,17 @@ export class Table extends ExcelComponent {
       document.onmouseup = () => {
         document.onmousemove = null;
 
-        listCellColumn.forEach((cell) => {
+        cells.forEach((cell) => {
           cell.style.borderRight = null;
         });
       }
     }
   }
 }
+
+// 53 ms  Scripting
+// 678 ms  Rendering
+// 315 ms  Painting
+// 196 ms  System
+// 4149 ms  Idle
+// 5392 ms  Total
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,23 +3,25 @@ const CODES = {
   Z: 90
 }
 
-function toCell(data) {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable data-name-column="${data}"></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, i) {
   return `
-    <div class="column" data-type="resizable">
+    <div class="column" data-type="resizable" data-col="${i}">
       ${col}
-      <div class="col-resize" data-resize="col" data-column="${col}"></div>
+      <div class="col-resize" data-resize="col""></div>
     </div>
   `
 }
 
 function createRow(i, content) {
-  const resize = i ? '<div class="row-resize" data-resize="row"></div>' : '';
+  const resize = i
+      ? '<div class="row-resize" data-resize="row" data-type="resizable"></div>'
+      : '';
   return `
     <div class="row">
       <div class="row-info">
